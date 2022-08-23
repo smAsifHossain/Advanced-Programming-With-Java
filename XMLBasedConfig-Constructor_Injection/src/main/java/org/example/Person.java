0b1d3bb89118f38ddd71364acd0cb95e2a588534
S M Asif Hossain <89404155+smAsifHossain@users.noreply.github.com>
2022-08-23 21:53:43 +0600

    XML based config, Constructor injection

---
+++ XMLBasedConfig-Constructor_Injection/src/main/java/org/example/Person.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Person {
+
+    private String name;
+    private int personId;
+
+    public Person(String name, int personId){
+        this.name = name;
+        this.personId = personId;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", personId=" + personId +
+                '}';
+    }
+}
