71c4098785cf7e99f3bcb3326405cea76ed5637e
S M Asif Hossain <89404155+smAsifHossain@users.noreply.github.com>
2022-08-23 21:53:15 +0600

    XML based Config, Setter Injection

---
+++ XMLBasedConfig-Setter_Injection/src/main/java/org/example/App.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+
+        //Instantiating a container
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:config.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext("org/example/config.xml");
+        Student student1 = (Student) context.getBean("student1");
+        //Student student1 = context.getBean("student1",Student.class);
+        System.out.println(student1);
+
+
+    }
+}
